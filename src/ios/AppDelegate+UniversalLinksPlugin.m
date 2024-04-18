cae9a399186c5a51ba22a9d2f519b16d4e625f8b
Cortney Thomas <cortney.thomas@1st.com>
2024-04-18 11:00:49 -0400

    app delegate logs

--- src/ios/AppDelegate+UniversalLinksPlugin.m
+++ src/ios/AppDelegate+UniversalLinksPlugin.m
@@ -6,6 +6,9 @@ static NSString *const PLUGIN_NAME = @"UniversalLinks";
 @implementation AppDelegate (UniversalLinks)
 
 - (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray *))restorationHandler {
+    NSLog(@"Cortney in app delegate one: %@", userActivity.webpageURL);
+    NSLog(@"Cortney in app delegate two: %@", userActivity.activityType);
+    NSLog(@"Cortney in app delegate three: %@", userActivity.targetContentIdentifier);
     // ignore activities that are not for Universal Links
     if (![userActivity.activityType isEqualToString:NSUserActivityTypeBrowsingWeb] || userActivity.webpageURL == nil) {
         return NO;
